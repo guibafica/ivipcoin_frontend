5d84561179a80d5ccbeaf24367ca0d935adb28df
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-06-14 15:46:37 -0300

    Fix task card styles

--- src/components/task-card.tsx
+++ src/components/task-card.tsx
@@ -1,6 +1,7 @@
-import * as Dialog from "@radix-ui/react-dialog";
 import { formatDistanceToNow } from "date-fns";
 import { X } from "lucide-react";
+import * as Dialog from "@radix-ui/react-dialog";
+import ptBR from "date-fns/locale/pt-BR";
 
 import { ITaskProps } from "../utils/interfaces/ITaskProps";
 
@@ -12,32 +13,42 @@ interface ITaskCardProps {
 export function TaskCard(props: ITaskCardProps) {
   return (
     <Dialog.Root>
-      <Dialog.Trigger className="rounded-md text-left flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+      <Dialog.Trigger className="rounded-md text-left flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-main-yellow">
         <span className="text-sm font-medium text-slate-300">
-          {formatDistanceToNow(props.payload.date)} atrás
+          {formatDistanceToNow(props.payload.date, {
+            addSufix: true, // @ts-expect-error 'formatDistanceToNow locale'
+            locale: ptBR,
+          })}{" "}
+          atrás
         </span>
 
-        <p className="text-sm leading-6 text-slate-400" key={Math.random()}>
-          {props.payload.body}
-        </p>
+        <p className="text-sm leading-6 text-slate-400">{props.payload.body}</p>
 
         <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
       </Dialog.Trigger>
 
       <Dialog.Portal>
-        <Dialog.Overlay className="inset-0 fixed bg-black/60" />
+        <Dialog.Overlay className="DialogTaskCardOverlay" />
 
-        <Dialog.Content className="overflow-hidden fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:h-[60vh] md:max-w-[640px] w-full bg-slate-700 md:rounded-md flex flex-col outline-none">
-          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100 transition-all">
+        <Dialog.Content className="DialogTaskCardContent">
+          <Dialog.Close className="DialogTaskCardClose">
             <X className="size-5" />
           </Dialog.Close>
 
           <div className="flex flex-1 flex-col gap-3 p-5">
-            <span className="text-sm font-medium text-slate-300">
-              {formatDistanceToNow(props.payload.date)} atrás
+            <span className="text-lg font-medium" style={{ color: "#cbd5e1" }}>
+              {formatDistanceToNow(props.payload.date, {
+                addSufix: true, // @ts-expect-error formatDistanceToNow locale
+                locale: ptBR,
+              })}{" "}
+              atrás
             </span>
 
-            <p className="text-sm leading-6 text-slate-400" key={Math.random()}>
+            {/* alterar para um textField */}
+            <p
+              className="text-base leading-6 text-slate-400"
+              style={{ color: "#94a3b8" }}
+            >
               {props.payload.body}
             </p>
           </div>
@@ -45,11 +56,12 @@ export function TaskCard(props: ITaskCardProps) {
           <button
             type="button"
             onClick={() => props.handleDeleteTask(props.payload.id)}
-            className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group hover:bg-slate-900 transition-all"
+            className="w-full py-4 text-center text-sm cursor-pointer outline-none font-medium group"
+            style={{ backgroundColor: "#1e293b", color: "#cbd5e1" }}
           >
             Deseja{" "}
             <span className="text-red-400 group-hover:underline">
-              deletar esta nota
+              deletar esta task
             </span>
             ?
           </button>
